f5a379b50aa3b716d8088cad0e162ae59b5f4ed8
Fauzaan <fauzaan.numaan@gmail.com>
2016-11-17 22:16:13 +0700

    Add files via upload

---
+++ Over.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Over : MonoBehaviour {
+
+	public ScoringManager scoring;
+	public bool over;
+	public GUIStyle style2;
+	public Texture image1;
+
+	// Use this for initialization
+	void Start () {
+		over = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.name == "Quad1"){
+			over = true;
+			Time.timeScale = 0;
+		}
+	}
+
+	void OnGUI ()
+	{
+		if (over==true) 
+		{
+			GUI.Label (new Rect (Screen.width * 0.3f, Screen.height * 0.45f, Screen.width * 0.75f, Screen.height * 0.25f), "Game Over !", style2);
+			GUI.Button (new Rect (Screen.width * 0.48f, Screen.height * 0.55f, 50, 50), image1) ;
+			//{
+				//ScoringManager.scoring.cekHighScore();
+				////Application.LoadLevel (0);
+				//Time.timeScale = 1;
+			//}
+		}
+	}
+}
---
+++ SpawnCactus.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCactus : MonoBehaviour {
+
+	public GameObject[] cactus;
+
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+
+
+	void Start () {
+		Spawn ();
+	}
+
+	void update()
+	{
+
+	}
+
+	void Spawn(){
+		GameObject clone = (GameObject)Instantiate (cactus[Random.Range(0,cactus.Length)],transform.position,Quaternion.identity);
+		clone.name = "Quad1";
+		clone.AddComponent<BoxCollider2D> ();
+		clone.GetComponent<BoxCollider2D>().isTrigger = true;
+		//float randomer = Random.Range (1, 5);
+		Invoke ("Spawn",Random.Range(spawnMin,spawnMax));
+	}
+}
