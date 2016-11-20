575f1dc30ffa156f80359f8ffaced5498cc33c60
kekresna <alvinkresna@gmail.com>
2016-11-20 09:36:16 +0700

    Add files via upload
    Yg udah nyatet High Score sama udah stop game over

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButton(0)) {
+			SceneManager.LoadScene ("Main Scene");
+		}
+	}
+}
